6c35692625eb447a2722661e117ee23c6cfb3182
Katherine Scott <kasc1625@gmail.com>
2023-02-01 17:04:50 -0800

    modified:   src/App.js 	new file:   src/Categories.js 	modified:   src/Menu.js 	modified:   src/data.js 	new file:   src/images/albacore.jpg 	new file:   src/images/cocoshrimp.jpg 	new file:   src/images/kingcrab.jpg 	new file:   src/images/miso.jpg 	new file:   src/images/rainbow.jpg 	new file:   src/images/sake.jpg 	new file:   src/images/spicysal.jpg 	new file:   src/images/spider.jpg 	new file:   src/images/tempshrimp.jpg 	new file:   src/images/yellowtail.jpg 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, {useState} from "react";
-import './index.css'
-import Menu from "./Menu";
-import items from './data'
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
-  const[menuItems, setMenuItems] = useState(items)
-  const[categories, setCategories] = useState([])
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -14,11 +24,12 @@ function App() {
           <h2>Kitty Kat's Sushi Menu</h2>
           <div className="underline"></div>
         </div>
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
-   
   );
 }
 
 export default App;
+
--- src/data.js
+++ src/data.js
@@ -1,70 +1,117 @@
 import dragon from './images/dragon.jpg'
 import hawaii from'./images/hawaii.jpg'
+import tempshrimp from './images/tempshrimp.jpg'
+import spicysal from './images/spicysal.jpg'
+import spider from './images/spider.jpg'
+import rainbow from './images/rainbow.jpg'
+import miso from './images/miso.jpg'
+import cocoshrimp from'./images/cocoshrimp.jpg'
+import kingcrab from './images/kingcrab.jpg'
+import sake from './images/sake.jpg'
+import yellowtail from './images/yellowtail.jpg'
+import albacore from './images/albacore.jpg'
+
 const menu =[
     {
         id: 1,
-        title: 'butternvlk pancakes',
-        category: 'breakfast',
-        price: 15.99,
+        title: 'Dragon Roll',
+        category: 'Rolls',
+        price: 22,
         img: dragon,
-        desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+        desc: `snow crab mix, avocado, cucumber topped w/ half an eel,
+        sesame seeds, tobiko, soy glaze `,
       },
       {
         id: 2,
-        title: 'diner double',
-        category: 'lunch',
-        price: 13.99,
+        title: 'Hawaii Roll',
+        category: 'Rolls',
+        price: 20,
         img: hawaii,
         desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
       },
       {
         id: 3,
-        title: 'godzilla milkshake',
-        category: 'shakes',
-        price: 6.99,
-        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+        title: 'Tempura Shrimp Roll',
+        category: 'Rolls',
+        price: 22,
+        img: tempshrimp,
+        desc: `shrimp tempura, mayo, avocado, cucumber, tobiko,
+        sesame seeds`,
       },
       {
         id: 4,
-        title: 'country delight',
-        category: 'breakfast',
-        price: 20.99,
-        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+        title: 'Spicy Salmon Roll',
+        category: 'Rolls',
+        price: 21,
+        img: spicysal,
+        desc: `salmon, spicy aioli, avocado, cucumber,
+        tobiko, sesame seed `,
       },
       {
         id: 5,
-        title: 'egg attack',
-        category: 'lunch',
-        price: 22.99,
-        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+        title: 'Spider Roll',
+        category: 'Rolls',
+        price: 22,
+        img: spider,
+        desc: `soft-shell crab, mayo, avocado, cucumber, tobiko`,
       },
       {
         id: 6,
-        title: 'oreo dream',
-        category: 'shakes',
-        price: 18.99,
-        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+        title: 'Rainbow Roll',
+        category: 'Rolls',
+        price: 20,
+        img: rainbow,
+        desc: `snow crab mix, cucumber topped w/ tuna, salmon,
+        shrimp, yellowtail, avocado, tobiko, spicy aioli`,
+      
       },
       {
         id: 7,
-        title: 'bacon overflow',
-        category: 'breakfast',
-        price: 8.99,
-        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+        title: 'Miso Soup',
+        category: 'Soup',
+        price: 7,
+        img: miso, 
+        desc: `tofu, scallions, seaweed `,
       },
       {
         id: 8,
-        title: 'american classic',
-        category: 'lunch',
-        price: 12.99,
-        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+        title: 'Spicy King Crab',
+        category: 'Soup',
+        price: 21,
+        img: kingcrab, 
+        desc: `king crab, onion, cilantro, jalapeno, habanero tobiko, bonito broth`,
       },
       {
         id: 9,
-        title: 'quarantine buddy',
-        category: 'shakes',
-        price: 16.99,
-        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+        title: 'Spicy Coconut Shrimp',
+        category: 'Soup',
+        price: 21,
+        img: cocoshrimp, 
+        desc: `shrimp, onion, cilantro, coconut milk, habanero tobiko, bonito broth`,
+      },
+      {
+        id: 10,
+        title: 'Sake (Salmon)',
+        category: 'Sashimi',
+        price: 18,
+        img: sake, 
+        desc: `3 slices $10, 6 slices $18`,
+      },
+      {
+        id: 11,
+        title: 'Hamachi (yellowtail)',
+        category: 'Sashimi',
+        price: 18 ,
+        img: yellowtail, 
+        desc: `3 slices $10, 6 slices $18`,
+      },
+      {
+        id: 12,
+        title: 'Shiro Maguro (albacore)',
+        category: 'Sashimi',
+        price: 2,
+        img: albacore,
+        desc: `4 slices $10, 6 slices $14`,
       },
 ]
 
